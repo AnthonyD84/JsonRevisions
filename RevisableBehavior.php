3aa2bfbf1def24f8e932b33d8dc0626f0f01d01f
AnthonyD84 <anthonydevito84@gmail.com>
2019-01-22 09:48:20 -0500

    add files by upload

---
+++ RevisableBehavior.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Model\Behavior;
+
+use ArrayObject;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\Event;
+use Cake\ORM\Behavior;
+use Cake\ORM\Entity;
+
+/**
+ * Revisable behavior
+ */
+class RevisableBehavior extends Behavior
+{
+    /**
+     * Default configuration.
+     *
+     * @var array
+     *  set numberOfRevisionsToKeep to 0 to save unlimited reversions.
+     */
+    protected $_defaultConfig = [
+        'revisionField' => 'revisions',
+        'omittedFields' => ['id','revisions', 'created', 'modified'],
+        'numberOfRevisionsToKeep' => 0,
+    ];
+
+    public function initialize(array $config)
+    {
+        // Some initialization code here
+    }
+
+    public function beforeSave(Event $event, EntityInterface $entity, ArrayObject $options)
+    {
+        if ($entity->isDirty()) {
+            $this->setRevision($entity);
+        }
+    }
+
+    private function setRevision(Entity $entity)
+    {
+        $currentRevisions = $this->removeOldRevisions($entity);
+        $newRevision = $this->prepareNewRevision($entity);
+        $currentRevisions[date("YmdHis")] = $newRevision;
+        return $entity->set($this->getConfig('revisionField'), $currentRevisions);
+    }
+
+    /***
+     * @return array
+     * prepares the the entity data to be saved as a revision
+     * The revision data that will be returned is in the format of
+     * ['20190120112352'=>[...]]
+     */
+    private function prepareNewRevision(Entity $entity)
+    {
+        $config = $this->getConfig();
+        $newRevisionData = $entity->toArray();
+        foreach ($config['omittedFields'] as $field) {
+            unset($newRevisionData[$field]);
+        }
+        return $newRevisionData;
+    }
+
+    /***
+     * @return mixed
+     * remove older reversions when the number of reversions
+     * match or excede that of of numberOfrevisionsToKeep.
+     */
+    private function removeOldRevisions(Entity $entity)
+    {
+        $currenEntity = $entity->toArray();
+        $currentRevisions = $currenEntity[$this->getConfig('revisionField')];
+        if (empty($currenEntity[$this->getConfig('revisionField')])) {
+            return $currentRevisions;
+        }
+
+        if ((count($currentRevisions) >= $this->getConfig('numberOfRevisionsToKeep')) && $this->getConfig('numberOfRevisionsToKeep') > 0) {
+            $remove = true;
+            while ($remove) {
+                if ((count($currentRevisions) < $this->getConfig('numberOfRevisionsToKeep')) && $this->getConfig('numberOfRevisionsToKeep') > 1) {
+                    $remove = false;
+                    break;
+                }
+                $keys = array_keys($currentRevisions);
+                ksort($keys);
+                unset($currentRevisions[reset($keys)]);
+                unset($keys[reset($keys)]);
+            }
+        }
+        return $currentRevisions;
+    }
+}
---
+++ RevisableTrait.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Model\Entity\Traits;
+
+trait RevisableTrait
+{
+
+    /***
+     * @return int|void
+     * return a count of current reversions for the entity
+     */
+    protected function _getNumberOfReversions()
+    {
+        return count($this->revisions);
+    }
+
+    /***
+     * @return mixed
+     * virtual field that returns the first reversion on the stack
+     * first reversion is oldest by nature
+     */
+    protected function _getFirstReversion()
+    {
+        return reset($this->revisions);
+    }
+
+    /***
+     * @return mixed
+     * virtual field to return the previously saved reversion on the stack
+     */
+    protected function _getPreviousReversion()
+    {
+        $previousIndex = $this->number_of_reversions - 2;
+        $i = 0;
+        foreach ($this->revisions as $reversion) {
+            if ($i == $previousIndex) {
+                return $reversion;
+            }
+            $i++;
+        }
+        return false;
+    }
+
+    /***
+     * @param $key
+     * @return $this|bool
+     *  re-marshal the current entity with data from a previous reversion by passing the key.
+     */
+    public function restoreReversionByKey($key)
+    {
+        if (!in_array($key, array_keys($this->revisions))) {
+            return false;
+        }
+        $oldData = $this->revisions[$key];
+        foreach ($oldData as $key => $value) {
+            $this->set($key, $value);
+        }
+        return $this;
+    }
+
+    /***
+     * @param $reversion
+     * @return $this|bool
+     *  re-marshal current entity with data from the specified reversion.
+     */
+    public function restoreReversion($reversion)
+    {
+        if (empty($reversion)) {
+            return false;
+        }
+        foreach ($reversion as $key => $value) {
+            $this->set($key, $value);
+        }
+        return $this;
+    }
+
+    /***
+     * @return RevisableTrait|bool
+     *  re-marshal the current entity with data from the oldest (first) reversion
+     */
+    public function restoreFirstReversion()
+    {
+        return $this->restoreReversion($this->first_reversion);
+    }
+
+    /***
+     * @return RevisableTrait|bool
+     */
+    public function restorePreviousreversion()
+    {
+        return $this->restoreReversion($this->previous_reversion);
+
+    }
+
+    /***
+     * clear out the revisions array.
+     */
+    public function deleteAllReversions()
+    {
+        $this->set('revisions', []);
+    }
+}
